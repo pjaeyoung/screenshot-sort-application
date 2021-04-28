e3837796a7057c6d89ea8c97487b926aedc54905
jeanDeluge <53405354+jeanDeluge@users.noreply.github.com>
2021-04-28 17:24:01 +0900

    [feat] vision api 통신성공

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,8 +1,4 @@
-import express from "express";
-
 const app: express.Application = express();
-const bodyParser = require('body-parser');
-
 app.get(
     "/",
     (req: express.Request, res: express.Response, next: express.NextFunction)=>{
