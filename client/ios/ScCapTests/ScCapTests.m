e3739dbf97fe72af27cc595ba64fd208c4b00422
TaekwanMin <51706290+TaekwanMin@users.noreply.github.com>
2021-04-30 18:53:59 +0900

    [FIX]: rename-project (#7)
    - sscap -> ScCap으로 수정 (폴더명 포함)
    - exception : com.sccap.sccap은 대문자로 안함 (? : 플레이스토어가 대문자를 허용 안합니다.)
    - client에 각 2개씩 있었던 main index 파일 & 메인 App 파일 삭제

--- client/ios/ScCapTests/ScCapTests.m
+++ client/ios/ScCapTests/ScCapTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface SsCapTests : XCTestCase
+@interface ScCapTests : XCTestCase
 
 @end
 
-@implementation SsCapTests
+@implementation ScCapTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
