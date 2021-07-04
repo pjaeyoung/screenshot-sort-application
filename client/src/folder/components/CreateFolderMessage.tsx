02aeda59e1e5ea89bc5aee15b661bdf388116bc9
pjaeyoung <devdodose@gmail.com>
2021-07-04 19:59:23 +0900

    fix: 온보딩 여부값을 인자로 받아 메세지 처리

--- client/src/folder/components/CreateFolderMessage.tsx
+++ client/src/folder/components/CreateFolderMessage.tsx
@@ -2,16 +2,25 @@ import * as React from 'react';
 import { Dimensions } from 'react-native';
 import styled from '@emotion/native';
 
-const CreateFolderMessage: React.FC = () => {
+interface ICreateFolderMessageProps {
+  isOnboardng?: boolean;
+}
+
+const CreateFolderMessage: React.FC<ICreateFolderMessageProps> = ({ isOnboardng = false }) => {
   return (
     <Wrapper>
-      <BoldText>분류 가능한 폴더가 없습니다 :(</BoldText>
+      <BoldText>{isOnboardng ? messages.onboarding : messages.default}</BoldText>
       <GreyText>+버튼을 탭하여 폴더를 생성해주세요</GreyText>
     </Wrapper>
   );
 };
 
 export default CreateFolderMessage;
+
+const messages = {
+  default: '분류 가능한 폴더가 없습니다 :(',
+  onboarding: '즉시 분류를 위한 폴더가 필요해요! 🗂',
+};
 
 const Wrapper = styled.View({
   position: 'absolute',
