af7b879de3c19c5ba9bade107e0b8b1009ae87cd
pjaeyoung <devdodose@gmail.com>
2021-08-09 23:30:08 +0900

    chore: eslint, prettier 설정

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "bracketSpacing": true,
+    "jsxBracketSameLine": true,
+    "singleQuote": true,
+    "trailingComma": "all",
+    "arrowParens": "always",
+    "useTabs": false,
+    "tabWidth": 2,
+    "semi": true,
+    "printWidth": 100
+  }
