da049deb6c3d1643dc1371835afc7263ce7bf74c
pjaeyoung <devdodose@gmail.com>
2021-08-16 18:38:46 +0900

    fix: FolderSvgs파일에서 folderSvgsDataArr 가져오기

--- src/shared/components/FolderSvgs.tsx
+++ src/shared/components/FolderSvgs.tsx
@@ -1,19 +1,16 @@
 import * as React from 'react';
 import FolderSvg from './FolderSvg';
-import { folderSvgsData } from '../constants';
+import { folderSvgsDataArr } from '../constants';
 import { IFolderSvgProps } from '../types';
 import { StyleProp, ViewStyle } from 'react-native';
 
-const FolderSvgCurry = (
-  svgData: IFolderSvgProps,
-): React.FC<{ borderColor: string; style?: StyleProp<ViewStyle> }> => ({
-  borderColor,
-  style,
-  children,
-}) => (
-  <FolderSvg borderColor={borderColor} style={style} {...svgData}>
-    {children}
-  </FolderSvg>
-);
+const FolderSvgCurry =
+  (svgData: IFolderSvgProps): React.FC<{ borderColor: string; style?: StyleProp<ViewStyle> }> =>
+  ({ borderColor, style, children }) =>
+    (
+      <FolderSvg borderColor={borderColor} style={style} {...svgData}>
+        {children}
+      </FolderSvg>
+    );
 
-export default folderSvgsData.map(svgData => FolderSvgCurry(svgData));
+export default folderSvgsDataArr.map(FolderSvgCurry);
--- src/sort/components/FolderSvgs.tsx
+++ src/sort/components/FolderSvgs.tsx
@@ -1,7 +1,8 @@
 import { FolderSvg } from '@/shared/components';
-import { folderSvgsData } from '@/shared/constants';
+import { folderSvgsDataArr } from '@/shared/constants';
 import withScaleAnimatedDropzone from './withScaleAnimatedDropzone';
 
-export default folderSvgsData.map(svgData =>
+/* eslint-disable */
+export default folderSvgsDataArr.map((svgData) =>
   withScaleAnimatedDropzone({ Component: FolderSvg, defaultSvgData: svgData, maxScale: 1.5 }),
 );
